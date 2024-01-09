d2bdb9c0e09d5a46a50d64f49e87ed45d221e930
Simona Sud <ssimona.sud@gmail.com>
2024-01-09 10:09:19 +0200

    users

--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@
 2. git + github
 3. npm
 4. express morgan cors nodemon(dev)
-5. pasirasyti basic express server kuris suksis and port 3000
+5.
--- src/db/db.sql
+++ src/db/db.sql
@@ -1,13 +1,13 @@
--- sukurti lentele - users
--- username, password, timestamp
+-- sukurti lentele - 'users'
+-- id, name, email, password, role_id
 
--- sukurti 3 irasus
-
-CREATE TABLE users 
-(
-  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
-  username VARCHAR(255) NOT NULL, 
-  password VARCHAR(255) NOT NULL, 
-  timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  PRIMARY KEY (user_id)
-) ENGINE = InnoDB;
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role_id INT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY unique_email (email),
+  FOREIGN KEY (role_id) REFERENCES roles(id)
+) ENGINE=InnoDB;
