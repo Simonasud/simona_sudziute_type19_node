5bd5a49fd43673eb05479ccb966bd46abc7bceeb
Simona Sud <ssimona.sud@gmail.com>
2024-01-09 16:41:16 +0200

    login++

--- front-end/js/login.js
+++ front-end/js/login.js
@@ -1,14 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('loginForm');
 
-  loginForm.addEventListener('submit', (event) => {
+  loginForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     const email = document.getElementById('email').value;
+    console.log('email  ===', email);
     const password = document.getElementById('password').value;
 
-    // Čia galite pridėti prisijungimo logiką, pavyzdžiui, siųsti užklausą į serverį
-    console.log('Email:', email);
-    console.log('Password:', password);
+    // Siunčiame užklausą į serverį
+    try {
+      const response = await fetch('http://localhost:3000/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Jei prisijungimas pavyko, išsaugome duomenis į localStorage
+        localStorage.setItem('userEmail', email);
+        localStorage.setItem('userRole', data.role); // Arba kitaip gauti vartotojo rolę iš duomenų
+
+        // Nukreipiame į kitą puslapį
+        window.location = 'shop.html';
+      } else {
+        console.error('Klaida prisijungiant:', data.message);
+      }
+    } catch (error) {
+      console.error('Klaida siunčiant užklausą:', error);
+    }
   });
 });
