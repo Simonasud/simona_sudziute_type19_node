8ea83ee8169b022510268ddc72c6d2fa9bf29944
Simona Sud <ssimona.sud@gmail.com>
2024-01-09 17:33:02 +0200

    4.3

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,19 +3,19 @@ const express = require('express');
 
 const router = express.Router();
 const { validateFields } = require('../middleware');
+
 const authController = require('../controllers/authController');
-const { register, login } = require('../controllers/authController');
 
 const requiredFieldsForRegister = ['name', 'email', 'password', 'role_id'];
 
-// registracijos routa
+// Registration route
 router.post(
   '/register',
   validateFields(requiredFieldsForRegister),
-  authController.register,
+  authController.register
 );
-router.post('/register', authController.register);
-router.post('/register', register);
-router.post('/login', login);
+
+// Login route
+router.post('/login', authController.login);
 
 module.exports = router;
