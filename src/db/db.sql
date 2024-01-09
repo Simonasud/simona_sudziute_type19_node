8940727215167a15650532b48cadfa367a8879bb
Simona Sud <ssimona.sud@gmail.com>
2024-01-09 10:11:28 +0200

    shop_items

--- src/db/db.sql
+++ src/db/db.sql
@@ -11,3 +11,18 @@ CREATE TABLE users (
   UNIQUE KEY unique_email (email),
   FOREIGN KEY (role_id) REFERENCES roles(id)
 ) ENGINE=InnoDB;
+
+-- sukurti lentele - 'shop_items'
+-- id, name, price, description, image, item_type_id
+
+CREATE TABLE shop_items (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  description TEXT,
+  image VARCHAR(255),
+  item_type_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (item_type_id) REFERENCES item_types(id)
+) ENGINE=InnoDB;
+
